3a2966b4888cbcfffb812f5522bb7b8016255c32
Vishma Pratim Das <77196878+VishmaPratimDasHere@users.noreply.github.com>
2024-10-13 16:19:23 +0530

    Create Dataloader.py
    Added dataloader backbone

---
+++ Dataloader.py
@@ -0,0 +1,12 @@
+class Dataloader:
+  def __init__(self):
+    return Dataloader_v1()
+  
+  class Dataloader_v1:
+    def __init__(self):
+      self.data=None
+
+    def draw_data(self):
+      # data sampling logic
+      return self.data
+    
